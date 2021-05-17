1a2816b2ea27dc560d8d4b869f87851a2bbbd30c
Roman Kozlov <romandkoz@gmail.com>
2021-05-17 12:04:17 +0300

    9

--- diplom/bestjob/js/script.js
+++ diplom/bestjob/js/script.js
@@ -3,3 +3,8 @@ form.addEventListener( "submit", function ( event ) {
     event.preventDefault();
     document.location = "completed.html";
 } );
+
+var strGET = window.location.search.replace( '?s=', '');
+var inpJob = document.getElementById('inpjob');
+inpJob.value = decodeURIComponent(strGET);
+console.log(strGET);
